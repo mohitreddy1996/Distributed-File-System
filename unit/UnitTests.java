018b76901577f8b1a0dc80bbe8bfdb7437b27e43
Mohit Reddy <mohitreddy1996@gmail.com>
2017-04-13 15:11:34 +0530

    Unit Tests added.

--- unit/UnitTests.java
+++ unit/UnitTests.java
@@ -1,6 +1,8 @@
 package unit;
 
-import test.*;
+import test.Series;
+import test.SeriesReport;
+import test.Test;
 
 /** Runs all unit tests on distributed filesystem components.
 
@@ -18,7 +20,21 @@ public class UnitTests
         // Create the test list, the series object, and run the test series.
         @SuppressWarnings("unchecked")
         Class<? extends Test>[]     tests =
-            new Class[] {conformance.common.PathTest.class};
+            new Class[] {
+                    conformance.common.PathTest.class,
+                    conformance.naming.ContactTest.class,
+                    conformance.naming.ListingTest.class,
+                    conformance.naming.CreationTest.class,
+                    conformance.naming.RegistrationTest.class,
+                    conformance.rmi.StubTest.class,
+                    conformance.rmi.SkeletonTest.class,
+                    conformance.rmi.ThreadTest.class,
+                    conformance.common.PathTest.class,
+                    conformance.rmi.SkeletonTest.class,
+                    conformance.rmi.StubTest.class,
+                    conformance.rmi.ConnectionTest.class,
+                    conformance.rmi.ThreadTest.class,
+            };
         Series                      series = new Series(tests);
         SeriesReport                report = series.run(3, System.out);
 
